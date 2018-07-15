ebfa8fd508204810c0ee7f1c6632a78389809365
Paul Dwamena <pauldwamena10@gmail.com>
2018-07-15 11:22:51 -0700

    created the geolocation for the pharmacy

--- api/models/GeoLocation.model.js
+++ api/models/GeoLocation.model.js
@@ -34,7 +34,38 @@ class GeoLocation{
         })
     }
 // get_pharmacy
+        async get_closest_pharmacy(coords, radius = 1000){
+            return this.get_pharmacies(coords, radius).then(coords=>{
+                return coords[0]
+            })
+        }
+
 // get_pharmacys
+        get_pharmacies(coords, radius = 1000){
+        const Pharmacy = require('../database/Pharmacy')
+
+        //Sets
+        let point = {
+            type: 'point', 
+            coordinates: [parseFloat(coords.lng), parseFloat(coords.lat)]
+        }
+
+        Pharmacy.aggregate([{
+            '$geoNear' : {
+                'near': point,
+                'spherical': true,
+                'distanceField': 'dist',
+                'maxDistance': radius || 5000
+            }
+        }]).then(doc=>{
+            if(doc) return doc
+            else {
+                this.get_closest_pharmacy(coords, radius * 2)
+            }
+        })
+    }
+
+
 // get_paramedics
 // user_last_location
 
